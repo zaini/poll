e32e5ddad2891ed7dcec4ca4c906adde6422b6d0
Ali Z <51179189+AliMZaini@users.noreply.github.com>
2020-06-23 01:03:07 +0100

    Form from state, add/remove options

--- poll/src/App.js
+++ poll/src/App.js
@@ -4,6 +4,33 @@ import './App.scss';
 export default class App extends React.Component{
     constructor(props) {
         super(props);
+
+        this.state = {
+            values: ["", "", ""]
+        }
+    }
+
+    addOption = () => {
+        this.setState({
+            values: [...this.state.values, ""]
+        })
+    }
+
+    removeOption = (index) => {
+        console.log(index);
+        let values = [...this.state.values];
+        values.splice(index, 1);
+        this.setState({
+            values: values
+        })
+    }
+
+    changeValue = (e) => {
+        let values = [...this.state.values];
+        values[e.target.name] = e.target.value;
+        this.setState({
+            values: values
+        })
     }
 
     render() {
@@ -11,10 +38,14 @@ export default class App extends React.Component{
             <div>
                 <h1>poll</h1>
                 <input type='text' placeholder="type question here" /> <br/>
-                <input type='text' placeholder="type option here" /> <br/>
-                <input type='text' placeholder="type option here" /> <br/>
-                <input type='text' placeholder="type option here" /> <br/>
-                <button>add option</button> <br/>
+                {
+                    this.state.values.map((value, index) => {
+                        let option = <input key={index} name={index} type='text' placeholder="type option here" value={value} onChange={this.changeValue}/>;
+                        let remove = <button onClick={() => this.removeOption(index)}>x</button>
+                        return [option, remove, <br/>]
+                    })
+                }
+                <button onClick={() => this.addOption()}>add option</button> <br/>
                 <button>add question</button> <br/>
                 <input type='text' placeholder="type password here" /> <br/>
             </div>
